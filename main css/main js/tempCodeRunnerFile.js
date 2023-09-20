23d6a7f2d98f57a20f48164c405ae96fe8f0e38e
satya2215 <satyaprakashpanda2017@gmail.com>
2023-09-21 02:04:54 +0530

    updated

---
+++ main css/main js/tempCodeRunnerFile.js
@@ -0,0 +1,18 @@
+let age=5
+age=Number.parseInt(age)
+age>=18?console.log("you can drive"):console.log("you cannot drive")
+
+const a = 10
+let b=8
+if(a==b)
+{
+  console.log("guess is correct")
+}
+else if(a>b)
+{
+  console.log("number is lesser")
+}
+else
+{
+  console.log("number is greater")
+}
